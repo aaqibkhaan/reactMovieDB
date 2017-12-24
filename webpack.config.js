77c0e910a39c93302aa26a8c1cea3bf380f6cefd
aaqibkhaan <aaqibrashid@hotmail.com>
2017-12-24 16:46:24 +0000

    added: eslint config

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require("path");
+const webpack = require("webpack");
+
+const config = {
+  context: __dirname,
+  entry: ["./src/index.jsx"],
+  devtool: "cheap-eval-source-map",
+  output: {
+    path: path.join(__dirname, "public"),
+    filename: "bundle.js",
+    publicPath: "/public/"
+  },
+  devServer: {
+    publicPath: "/public/",
+    historyApiFallback: true
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json"]
+  },
+  stats: {
+    colors: true,
+    reasons: true,
+    chunks: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        enforce: "pre",
+        test: /\.jsx?$/,
+        loader: "eslint-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+      }
+    ]
+  }
+};
+
+if (process.env.NODE_ENV === "production") {
+  config.entry = "./src/index.jsx";
+  config.devtool = false;
+  config.plugins = [];
+}
+
+module.exports = config;
