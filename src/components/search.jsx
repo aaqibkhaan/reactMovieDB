5c31e6f9f43f15d3eeb7b6a9e69593e3465f609a
aaqibkhaan <aaqibrashid@hotmail.com>
2018-01-19 14:15:25 +0000

    added: search component

---
+++ src/components/search.jsx
@@ -0,0 +1,20 @@
+/*a2df3d1a7611194432bbdf1fc80540f2
+*/
+
+import React, { Component } from "react";
+
+class apiRequest extends Component {
+	state = {
+		searchTerm: ""
+	}
+
+	componentDidMount(){
+		
+	}
+
+	render() {
+		return <div>Hey its me</div>;
+	}
+}
+
+export default apiRequest;
