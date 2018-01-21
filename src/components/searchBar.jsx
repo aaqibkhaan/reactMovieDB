85459c330c27e5bd35452f61be5edf05b950f4dc
aaqibkhaan <aaqibrashid@hotmail.com>
2018-01-21 17:58:22 +0000

    added: searchBar component

---
+++ src/components/searchBar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const searchBar = () => (
+	<div>
+		<div>Hello this is searchBar</div>
+		<form>
+			{" "}
+			<input className="inputSearch" placeholder="search Movie Id" />
+		</form>
+	</div>
+);
+
+export default searchBar;
