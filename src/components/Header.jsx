0ffc97339863a7404410c7561714b182be3ee97d
aaqibkhaan <aaqibrashid@hotmail.com>
2018-01-23 11:40:45 +0000

    updated: searchBar component name updated to Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+// @flow
+
+import React from "react";
+import { Navbar, FormGroup, FormControl, Image } from "react-bootstrap";
+
+import TMDBlogo from "../images/movie_logo.svg";
+
+const imgStyle = {
+	height: "150%",
+	width: "auto",
+	paddingBottom: "10px",
+	paddingTop: "10px",
+	marginTop: "-8px",
+	display: "inline-block"
+};
+const Header = () => (
+	<Navbar bsStyle="inverse">
+		<Navbar.Header>
+			<Navbar.Brand>
+				<a href="#home">
+					<Image style={imgStyle} src={TMDBlogo} />
+				</a>
+			</Navbar.Brand>
+		</Navbar.Header>
+		<Navbar.Form pullRight>
+			<FormGroup>
+				<FormControl type="text" placeholder="Search Movie by ID" />
+			</FormGroup>{" "}
+		</Navbar.Form>
+	</Navbar>
+);
+
+export default Header;
