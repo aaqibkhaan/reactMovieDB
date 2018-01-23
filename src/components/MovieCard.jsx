ffc1b075efff910ec034ce89372361c09d28beae
aaqibkhaan <aaqibrashid@hotmail.com>
2018-01-23 13:17:51 +0000

    updated: MovieCard component prop-types added

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import { shape , string } from 'prop-types';
 
-/* eslint-disable*/
 const MovieCard = (props) => (
-      <div key= {props.movie.id} className="show-card"> 
+      <div className="show-card"> 
         <img alt={`${props.movie.title} Movie Poster`} src={`https://image.tmdb.org/t/p/w500/${props.movie.poster_path}`} />
         <h3>{props.movie.title}</h3>
         <h4>{props.movie.overview}</h4>
       </div>
 	)
 
+MovieCard.propTypes = {
+	movie: shape({
+		title: string.isRequired,
+		poster_path: string.isRequired,
+		overview: string.isRequired
+	}).isRequired
+}
 
 
 export default MovieCard;
