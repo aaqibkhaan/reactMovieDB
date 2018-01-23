eea47f9a54eb9f916f96ec9b0271ea5b0c9588ee
aaqibkhaan <aaqibrashid@hotmail.com>
2018-01-23 17:43:52 +0000

    added: styled-components applied to MovieCard Component

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import { shape , string } from 'prop-types';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+width: 32%;
+border: 1px solid #c1c1c1;
+border-radius: 4px;
+margin-bottom: 25px;
+padding-right: 10px;
+overflow: hidden;
+display: inline-block;
+max-width: 900px;
+`;
 
 const MovieCard = (props) => (
-      <div className="show-card"> 
-        <img alt={`${props.movie.title} Movie Poster`} src={`https://image.tmdb.org/t/p/w500/${props.movie.poster_path}`} />
+      <Wrapper> 
+        <img className="card-image" alt={`${props.movie.title} Movie Poster`} src={`https://image.tmdb.org/t/p/w500/${props.movie.poster_path}`} />
+        <div className="card-details">
         <h3>{props.movie.title}</h3>
         <h4>{props.movie.overview}</h4>
-      </div>
+        </div>
+      </Wrapper>
 	)
 
 MovieCard.propTypes = {
