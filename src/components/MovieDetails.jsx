f576d0bc9829f2d7bc4c28b48a75dde01961149e
aaqibkhaan <aaqibrashid@hotmail.com>
2018-01-27 16:05:13 +0000

    updated: MovieDetails component updated with working example

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { Component } from "react";
+import axios from "axios";
+import { URL_DETAIL, API_KEY } from "../const";
 
-const MovieDetails = () => (
-
-	<div> Inisde Movie Details</div>
-
-	)
-
+class MovieDetails extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			movieData: " "
+		};
+	}
+	componentDidMount() {
+		axios
+			.get(
+				`${URL_DETAIL}321612${API_KEY}&language=en-US&page=1`
+			)
+			.then(response => {
+				this.setState({ movieData: response.data });
+			});
+	}
+	render() {
+		const movieHere = this.state.movieData;
+// eslint-disable-next-line no-console
+		console.log(movieHere);
+		return <div> Title of the movie is {this.state.movieData.original_title} </div>;
+	}
+}
 export default MovieDetails;
