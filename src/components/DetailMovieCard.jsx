8e96f52026bf90d00187db62b4e2f94fa363b3c5
aaqibkhaan <aaqibrashid@hotmail.com>
2018-01-29 17:33:14 +0000

    updated: DetailMovieCard Row and Col react-bootstrap

--- src/components/DetailMovieCard.jsx
+++ src/components/DetailMovieCard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Glyphicon, Button } from 'react-bootstrap';
+import { Row, Col, Glyphicon, Button } from "react-bootstrap";
 import { shape, string } from "prop-types";
 import styled from "styled-components";
 import { URL_IMAGE } from "../const";
@@ -10,7 +10,6 @@ const Wrapper = styled.div`
 	margin-right: auto;
 	margin-left: auto;
 	margin-top: 5%;
-
 `;
 
 const Image = styled.img`
@@ -25,13 +24,38 @@ const DetailMovieCard = props => (
 		<div className="movie-details">
 			<h3>Title of the movie is {props.movie.original_title} </h3>
 			<ul className="item-list">
-				<li><Button bsSize="large"><Glyphicon className="green" glyph="star" />{ props.movie.budget} </Button> </li>
-				<li><Button bsSize="large"><Glyphicon glyph="heart" /> { props.movie.revenue} </Button> </li>
-				<li><Button bsSize="large"><Glyphicon glyph="heart" /> { props.movie.vote_count} </Button> </li>
+				<li>
+					<Button bsSize="large">
+						<Glyphicon className="green" glyph="star" />
+						{props.movie.budget}{" "}
+					</Button>{" "}
+				</li>
+				<li>
+					<Button bsSize="large">
+						<Glyphicon glyph="heart" /> {props.movie.revenue}{" "}
+					</Button>{" "}
+				</li>
+				<li>
+					<Button bsSize="large">
+						<Glyphicon glyph="heart" /> {props.movie.vote_count}{" "}
+					</Button>{" "}
+				</li>
 			</ul>
-			<div> 
-			<div className="movie-tagline"> {props.movie.tagline} </div>
-			<div className="movie-overview">{props.movie.movie-overview} </div>
+			<div>
+				<div className="movie-tagline"> {props.movie.tagline} </div>
+				<div className="movie-overview">{props.movie.overview} </div>
+			</div>
+			<div className="movie-subdetails">
+				<Row className="show-grid">
+					<Col xs={6} md={6}>
+					Realease Date :
+					<span className="movie-metadata"> {props.movie.release_date}</span>
+					</Col>
+					<Col xs={6} md={6}>
+						Running Time :
+						<span className="movie-metadata"> {props.movie.runtime} mins</span>
+					</Col>
+				</Row>{" "}
 			</div>
 		</div>
 	</Wrapper>
