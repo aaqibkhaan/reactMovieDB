042a0b83cf0599b4c260e848b63a546584365136
aaqibkhaan <aaqibrashid@hotmail.com>
2018-01-31 19:29:30 +0000

    added: MovieCard component Link

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
-import { shape , string } from 'prop-types';
-import styled from 'styled-components';
+// @flow
 
-const Wrapper = styled.div`
+import React from 'react';
+import { shape , string, number } from 'prop-types';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom'
+
+const Wrapper = styled((Link: any))`
 width: 42%;
 border: 1px solid #c1c1c1;
 border-radius: 4px;
@@ -12,10 +15,11 @@ margin-left: 40px;
 overflow: hidden;
 display: inline-block;
 max-width: 900px;
-`;
+background-color: #fff;
+ text-decoration: none;`;
 
 const MovieCard = (props) => (
-      <Wrapper> 
+      <Wrapper to= {`/movie/${props.movie.id}`}> 
         <img className="card-image" alt={`${props.movie.title} Movie Poster`} src={`https://image.tmdb.org/t/p/w500/${props.movie.poster_path}`} />
         <div className="card-details">
         <h3>{props.movie.title}</h3>
@@ -28,7 +32,8 @@ MovieCard.propTypes = {
 	movie: shape({
 		title: string.isRequired,
 		poster_path: string.isRequired,
-		overview: string.isRequired
+		overview: string.isRequired,
+    id: number.isRequired
 	}).isRequired
 }
 
