256ffed0893e810f2f86fb1705fc96d777ce9bff
aaqibkhaan <aaqibrashid@hotmail.com>
2018-02-02 12:04:32 +0000

    updated : few more chracteristics added

--- src/components/DetailMovieCard.jsx
+++ src/components/DetailMovieCard.jsx
@@ -1,15 +1,17 @@
-import React, { Component }from "react";
+import React, { Component } from "react";
 import { Row, Col, Glyphicon, Button } from "react-bootstrap";
-import { shape, string } from "prop-types";
 import styled from "styled-components";
-import { URL_IMAGE , URL_BACKGROUND } from "../const";
+import { URL_IMAGE, URL_BACKGROUND } from "../const";
 
 const Wrapper = styled.div`
-	max-width: 50%;
+	max-width: 60%;
 	overflow: hidden;
 	margin-right: auto;
 	margin-left: auto;
 	margin-top: 5%;
+	border-radius: 3px;
+	background: rgba(0, 0, 0, 0.85);
+	color: white;
 `;
 
 const Image = styled.img`
@@ -19,66 +21,87 @@ const Image = styled.img`
 `;
 
 class DetailMovieCard extends Component {
+	/* eslint-disable */
 
-
-/* eslint-disable */
 	render() {
-		const { poster_path , original_title , backdrop_path } = this.props.movie;
+		// eslint-disable no-console
+		console.log(this.props.movie);
+		const {
+			poster_path,
+			original_title,
+			backdrop_path,
+			vote_average,
+			vote_count,
+			tagline,
+			overview,
+			release_date,
+			budget,
+			revenue,
+			runtime
+		} = this.props.movie;
 
 		return (
 			<Wrapper>
-		<Image alt="" src={`${URL_IMAGE}${poster_path}`} />
-		<div className="movie-details">
-			<h3>Title of the movie is {original_title} </h3>
-			<ul className="item-list">
-				<li>
-					<Button bsSize="large">
-						<Glyphicon className="green" glyph="star" />
-						{this.props.movie.budget}{" "}
-					</Button>{" "}
-				</li>
-				<li>
-					<Button bsSize="large">
-						<Glyphicon glyph="heart" /> {this.props.movie.revenue}{" "}
-					</Button>{" "}
-				</li>
-				<li>
-					<Button bsSize="large">
-						<Glyphicon glyph="heart" /> {this.props.movie.vote_count}{" "}
-					</Button>{" "}
-				</li>
-			</ul>
-			<div>
-				<div className="movie-tagline"> {this.props.movie.tagline} </div>
-				<div className="movie-overview">{this.props.movie.overview} </div>
-			</div>
-			<div className="movie-subdetails">
-				<Row className="show-grid">
-					<Col xs={6} md={6}>
-					Realease Date :
-					<span className="movie-metadata"> {this.props.movie.release_date}</span>
-					</Col>
-					<Col xs={6} md={6}>
-						Running Time :
-						<span className="movie-metadata"> {this.props.movie.runtime} mins</span>
-					</Col>
-				</Row>{" "}
-			</div>
-		</div>
-	</Wrapper>
-)
+				<Image alt="" src={`${URL_IMAGE}${poster_path}`} />
+				<div className="movie-details">
+					<h3>Title of the movie is {original_title} </h3>
+					<ul className="item-list">
+						<li>
+								<Glyphicon className="green" glyph="star" />{" "}
+								{vote_average}
+						</li>
+						<li>
+								<Glyphicon className="green"  glyph="heart" /> {vote_count}
+						</li>
+					</ul>
+					<div>
+						<div className="movie-tagline"> {tagline} </div>
+						<div className="movie-overview">{overview} </div>
+					</div>
+					<div className="movie-subdetails">
+						<Row className="show-grid1">
+							<Col xs={6} md={6}>
+								Realease Date :
+								<span className="movie-metadata">
+									{" "}
+									{release_date}
+								</span>
+							</Col>
+							<Col xs={6} md={6}>
+								Running Time :
+								<span className="movie-metadata">
+									{" "}
+									{runtime} mins
+								</span>
+							</Col>
+						</Row>
+						<Row className="show-grid2">
+							<Col xs={6} md={6}>
+								Budget:
+								<span className="movie-metadata">
+									{" "}
+									$ {budget}{" "}
+								</span>
+							</Col>
+							<Col xs={6} md={6}>
+								Revenue:
+								<span className="movie-metadata">
+									{" "}
+									$ {revenue}
+								</span>
+							</Col>
+						</Row>
+					</div>
+				</div>
+			</Wrapper>
+		);
 	}
-  componentDidUpdate() {
-    document.body.style.backgroundImage =  `url(${URL_BACKGROUND}${this.props.movie.backdrop_path})`;
-    console.log(this.props.movie.backdrop_path);
-  }
-
+	componentDidUpdate() {
+		document.body.style.backgroundImage = `url(${URL_BACKGROUND}${
+			this.props.movie.backdrop_path
+		})`;
+		console.log(this.props.movie.backdrop_path);
+	}
 }
-
-DetailMovieCard.propTypes = {
-	movie: shape({
-		original_title: string.isRequired
-	}).isRequired
-};
 
 export default DetailMovieCard;
