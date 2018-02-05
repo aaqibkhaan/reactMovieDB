f56c0e45114568d8fb077444d2c0628aa99d6944
aaqibkhaan <aaqibrashid@hotmail.com>
2018-02-05 14:09:43 +0000

    added: styled-components added for bootstrap along

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,21 @@
 import React, { Component } from "react";
-import { Navbar, Image } from 'react-bootstrap/lib'
+import styled from "styled-components";
+import { Navbar } from 'react-bootstrap/lib'
 import TMDBlogo from "../images/movie_logo.svg";
+
+  const Brand = styled.span`
+    fontWeight: bold;
+    textTransform: caplitalize;
+    paddingLeft: 10;
+    fontSize: 1.2em;`;
+
+  const Image = styled.img`
+    height: 100%;
+    width: auto;
+    paddingLeft: 10px;
+    marginTop: -8px;
+    display: inline-block;`
+  ;
 
 class Search extends Component {
   constructor(props) {
@@ -17,13 +32,13 @@ class Search extends Component {
         <Navbar.Header>
           <Navbar.Brand>
             <a href="/">
-              <span>Brand</span>
-              <Image src={TMDBlogo} />
+            <Brand>{this.props.brand}</Brand>
+              <Image alt = " " src={TMDBlogo}/>
             </a>
           </Navbar.Brand>
         </Navbar.Header>
         <Navbar.Form pullRight>
-        <input />
+<input />
         </Navbar.Form>
       </Navbar>
     );
