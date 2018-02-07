fb46d3dfa6d8ae95865633712de36109971022ed
aaqibkhaan <aaqibrashid@hotmail.com>
2018-02-07 19:31:31 +0000

    updated: Browser Router Updated with HashRouter

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom';
 import App from './components/app';
 
 const appID = (document.getElementById("app"));
 const renderApp = () => {
 	render(
-		<BrowserRouter>
+		<HashRouter>
 			<App />
-		</BrowserRouter> , appID );
+		</HashRouter> , appID );
 }
 
 renderApp();
