4e6e14581ef9323537fb9efb026d68b11140c39d
aaqibkhaan <aaqibrashid@hotmail.com>
2018-02-09 14:06:10 +0000

    deleted: react-lines-ellipsis , css class added instead

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,23 +5,18 @@ import { shape , string, number } from 'prop-types';
 import styled from 'styled-components';
 import { Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import LinesEllipsis from 'react-lines-ellipsis';
-import responsiveHOC from 'react-lines-ellipsis/lib/responsiveHOC'
 
-const ResponsiveEllipsis = responsiveHOC()(LinesEllipsis);
 
 const Wrapper = styled((Link: any))`
 
-border: 1px solid #c1c1c1;
 border-radius: 4px;
 margin-bottom: 25px;
 padding-right: 10px;
-margin-left: 40px;
 overflow: hidden;
 display: inline-block;
-max-width: 900px;
 background-color: #fff;
  text-decoration: none;`;
+
 
 const MovieCard = (props) => (
       <Col xs={12} sm={12} md={6} lg={6}> 
@@ -29,13 +24,7 @@ const MovieCard = (props) => (
         <img className="card-image" alt={`${props.movie.title} Movie Poster`} src={`https://image.tmdb.org/t/p/w500/${props.movie.poster_path}`} />
         <div className="card-details">
         <h3>{props.movie.title}</h3>
-       <ResponsiveEllipsis className="ellipsis"
-          text={props.movie.overview}
-          maxLine='5'
-          ellipsis='...'
-          trimRight
-          basedOn='letters'
-        />
+        <p className="text-ellipsis"> {props.movie.overview} </p>
         </div>
       </Wrapper>
       </Col>)
